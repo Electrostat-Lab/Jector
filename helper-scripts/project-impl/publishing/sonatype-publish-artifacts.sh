abee3f07a0d17c57ad0c1b645e54eef9ee15aab7
pavl_g <scrappers.tm@gmail.com>
2023-07-29 12:27:27 +0300

    github-workflows: addded jector-monkey to the publishing scripts

--- helper-scripts/project-impl/publishing/sonatype-publish-artifacts.sh
+++ helper-scripts/project-impl/publishing/sonatype-publish-artifacts.sh
@@ -5,13 +5,17 @@ source "./helper-scripts/project-impl/variables.sh"
 
 # obtain dependencies in the form 'groupId:artifact:version'
 version=${1}
-desktop_artifact="${build_dir}/${desktop_artifactId_release}-${version}.jar"
 
-sources_jar="${build_dir}/${desktop_artifactId_release}-${version}-sources.jar"
-javadoc_jar="${build_dir}/${desktop_artifactId_release}-${version}-javadoc.jar"
+lib_artifact="${lib_build_dir}/${lib_artifactId_release}-${version}.jar"
+lib_sources_jar="${lib_build_dir}/${lib_artifactId_release}-${version}-sources.jar"
+lib_javadoc_jar="${lib_build_dir}/${lib_artifactId_release}-${version}-javadoc.jar"
+
+ext_artifact="${ext_build_dir}/${ext_artifactId_release}-${version}.jar"
+ext_sources_jar="${ext_build_dir}/${ext_artifactId_release}-${version}-sources.jar"
+ext_javadoc_jar="${ext_build_dir}/${ext_artifactId_release}-${version}-javadoc.jar"
 
 generateGenericPom "${groupId}" \
-                   "${desktop_artifactId_release}" \
+                   "${lib_artifactId_release}" \
                    "${version}" \
                    "The Jector Framework" \
                    "A Dependency Injection framework designed for JVM and Android applications with an Open-Ended API and Submit-Callback features." \
@@ -23,7 +27,23 @@ generateGenericPom "${groupId}" \
                    "The AvrSandbox" \
                    "https://github.com/Software-Hardware-Codesign" \
                    "scm:git:git://github.com/Software-Hardware-Codesign/Jector.git" \
-                   "${desktop_pomFile}"
+                   "${lib_pomFile}"
+
+generateGenericPom "${groupId}" \
+                   "${ext_artifactId_release}" \
+                   "${version}" \
+                   "The JectorMonkey API" \
+                   "A Specialized implementation of the Jector Framework for jMonkeyEngine." \
+                   "https://github.com/Software-Hardware-Codesign/Jector" \
+                   "The AvrSandbox Project, Jector Framework, BSD-3 Clause License" \
+                   "https://github.com/Software-Hardware-Codesign/Jector/blob/master/LICENSE" \
+                   "Pavly Gerges (aka. pavl_g)" \
+                   "pepogerges33@gmail.com" \
+                   "The AvrSandbox" \
+                   "https://github.com/Software-Hardware-Codesign" \
+                   "scm:git:git://github.com/Software-Hardware-Codesign/Jector.git" \
+                   "${ext_pomFile}"
 
 # publish 'android' and 'desktop' builds to maven sonatype
-publishBuild "${desktop_artifactId_release}" "${desktop_artifact}" "${version}" "${javadoc_jar}" "${sources_jar}" "${desktop_pomFile}"
+publishBuild "${lib_artifactId_release}" "${lib_artifact}" "${version}" "${lib_javadoc_jar}" "${lib_sources_jar}" "${lib_pomFile}"
+publishBuild "${ext_artifactId_release}" "${ext_artifact}" "${version}" "${ext_javadoc_jar}" "${ext_sources_jar}" "${ext_pomFile}"
